706166680cb88c1103de4fc9abee80946fdc4897
Manvan33 <rikostik@gmail.com>
2022-08-08 15:14:54 +0200

    WIP: Refactoring everything

--- script.js
+++ script.js
@@ -1,117 +1,131 @@
 function getUrlVars() {
-	var vars = {};
-	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-		vars[key] = value.split("#")[0];
-	});
-	return vars;
+    let vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value.split("#")[0];
+    });
+    return vars;
 }
-var colors = ['red', 'green', 'blue'];
-var players = [];
-var teams = [];
-var res = {};
+
+let players = [];
+let teams = [];
+let res = {};
+
 function init() {
-	if (getUrlVars()["players"] != null) {
-		if (!isNaN(getUrlVars()["players"])) {
-			for (i = 1; i <= parseInt(getUrlVars()["players"]); i++) {
-				players.push("Player " + i);
-			}
-		} else {
-			players = getUrlVars()["players"].split(",")
-		}
-	}
-	if (getUrlVars()["teams"] != null) {
-		if (!isNaN(getUrlVars()["teams"])) {
-			for (i = 1; i <= parseInt(getUrlVars()["teams"]); i++) {
-				teams.push("Team " + i);
-			}
-		} else {
-			teams = getUrlVars()["teams"].split(",")
-		}
-	}
-	$("#playerInput").keyup(function (e) {
-		if (e.which == 13) {
-			addPlayer();
-		}
-	});
-	$("#teamInput").keyup(function (e) {
-		if (e.which == 13) {
-			addTeam();
-		}
-	});
-	$("#teamAdd").click(function () {
-		addTeam();
-	});
-	$("#playerAdd").click(function () {
-		addPlayer();
-	});
-	$("#launch").click(function () {
-		randomize();
-	});
-	updatePlayers();
-	updateTeams();
+    if (getUrlVars()["players"] != null) {
+        if (!isNaN(getUrlVars()["players"])) {
+            for (i = 1; i <= parseInt(getUrlVars()["players"]); i++) {
+                players.push("Player " + i);
+            }
+        } else {
+            players = getUrlVars()["players"].split(",")
+        }
+    }
+    if (getUrlVars()["teams"] != null) {
+        if (!isNaN(getUrlVars()["teams"])) {
+            for (i = 1; i <= parseInt(getUrlVars()["teams"]); i++) {
+                teams.push("Team " + i);
+            }
+        } else {
+            teams = getUrlVars()["teams"].split(",")
+        }
+    }
+    document.getElementById("playerInput").addEventListener("keyup", function (e) {
+        if (e.key === "Enter") {
+            addPlayer();
+        }
+    });
+    document.getElementById("teamInput").addEventListener("keyup", e => {
+        if (e.key === "Enter") {
+            addTeam();
+        }
+    });
+    document.getElementById("teamAdd").addEventListener("click", e => {
+        addTeam();
+    });
+    document.getElementById("playerAdd").addEventListener("click", e => {
+        addPlayer();
+    });
+    document.getElementById("launch").addEventListener("click", e => {
+        randomize();
+    });
+    updatePlayers();
+    updateTeams();
 }
+
 function addTeam() {
-	$("#teamInput").val().length == 0 ? teams.push("Team " + (teams.length + 1)) : teams.push($("#teamInput").val());
-	$("#teamInput").val("");
-	updateTeams();
+    document.getElementById("teamInput").value.length === 0 ? teams.push("Team " + (teams.length + 1)) : teams.push(document.getElementById("teamInput").value);
+    document.getElementById("teamInput").value = "";
+    updateTeams();
 }
+
 function addPlayer() {
-	$("#playerInput").val().length == 0 ? players.push("Player " + (players.length + 1)) : players.push($("#playerInput").val());
-	$("#playerInput").val("");
-	updatePlayers();
+    document.getElementById("playerInput").value.length === 0 ? players.push("Player " + (players.length + 1)) : players.push(document.getElementById("playerInput").value);
+    document.getElementById("playerInput").value = "";
+    updatePlayers();
 }
+
+function linode(text, classes) {
+    let node = document.createElement("li");
+    node.innerText = text;
+    node.setAttribute("class", classes);
+    return node;
+}
+
 function updatePlayers() {
-	$("#playersList")[0].innerHTML = "";
-	if (players.length != 0) {
-		for (i of players) {
-			$("#playersList").append('<li class="playerItem">' + i + "</li>");
-		}
-	}
-	$(".playerItem").click(function () {
-		players.splice(players.indexOf($(this).text()), 1);
-		updatePlayers();
-	});
+    document.getElementById("playersList").innerText = "";
+    if (players.length !== 0) {
+        for (i of players) {
+            document.getElementById("playersList").appendChild(linode(i, "playerItem"));
+        }
+    }
+    document.querySelectorAll(".playerItem").forEach(item => item.addEventListener("click", e => {
+        players.splice(players.indexOf(e.currentTarget.innerText), 1);
+        updatePlayers();
+    }));
 }
+
 function updateTeams() {
-	$("#teamsList")[0].innerHTML = "";
-	if (teams.length != 0) {
-		for (i of teams) {
-			$("#teamsList").append('<li class="teamItem">' + i + "</li>");
-		}
-	}
-	$(".teamItem").click(function () {
-		teams.splice(teams.indexOf($(this).text()), 1);
-		updateTeams();
-	});
+    document.getElementById("teamsList").innerText = "";
+    if (teams.length !== 0) {
+        for (i of teams) {
+            document.getElementById("teamsList").appendChild(linode(i, "teamItem"));
+        }
+    }
+    document.querySelectorAll(".teamItem").forEach(item => item.addEventListener("click", e => {
+        teams.splice(teams.indexOf(e.currentTarget.innerText), 1);
+        updateTeams();
+    }));
 }
+
 function randomize() {
-	pool = Array.from(players);
-	res = {};
-	for (e of teams) {
-		res[e] = [];
-	}
-	i = 0;
-	while (pool.length) {
-		rand = Math.floor(Math.random() * pool.length);
-		res[teams[i]].push(pool[rand]);
-		pool.splice(rand, 1);
-		i++
-		if (i == teams.length) {
-			i = 0;
-		}
-	}
-	showRes();
+    let pool = Array.from(players);
+    res = {};
+    for (e of teams) {
+        res[e] = [];
+    }
+    let i = 0;
+    while (pool.length) {
+        let rand = Math.floor(Math.random() * pool.length);
+        res[teams[i]].push(pool[rand]);
+        pool.splice(rand, 1);
+        i++;
+        if (i === teams.length) {
+            i = 0;
+        }
+    }
+    showRes();
 }
+
 function showRes() {
-	output = '';
-	for (e of teams) {
-		output += '<ul class="ResTeam"><li>' + e + '</li>';
-		for (i of res[e]) {
-			output += '<li>' + i + '</li>';
-		}
-		output += '</ul>';
-	};
-	$("#results")[0].innerHTML = output;
+    let output = '';
+    for (e of teams) {
+        output += '<ul class="ResTeam"><li>' + e + '</li>';
+        for (i of res[e]) {
+            output += '<li>' + i + '</li>';
+        }
+        output += '</ul>';
+    }
+    document.getElementById("results").innerHTML = output;
 }
 
 
